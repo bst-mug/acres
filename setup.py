a680257f1267145565570e1d5894e8e386032266
Michel Oleynik <michelole@gmail.com>
2018-02-09 11:31:25 +0100

    Adds basic package structure

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='acres',
+    version='0.1',
+    description='Acronym resolution',
+    url='https://github.com/bst-mug/acres',
+    author='Stefan Schulz',
+    author_email='stefan.schulz@medunigraz.at',
+    license='Apache License 2.0',
+    packages=['acres']
+)
