2689e6a3f5f47a7d7e6bd86990e71c9970e82f27
Michel Oleynik <michelole@gmail.com>
2018-02-15 21:13:40 +0100

    Adds all imports
    This ensures imports are always correct when running unit tests.
    
    Currently broken and executing code is commented out.

--- tests/context.py
+++ tests/context.py
@@ -8,6 +8,8 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')
 from acres import ExpansionOfAcronyms
 from acres import Filters
 #from acres import FindSynonymForAcronymsInNgrams
-from acres import ling
 from acres import Functions
+from acres import GetAcronymsFromWeb
+from acres import GetSynonymsFromNgrams
+from acres import ling
 from acres import RateAcronymResolutions
