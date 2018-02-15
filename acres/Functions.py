888bc98987562c01ed38281a7a466ebcad87170c
Stefan Schulz <stefan.schulz@medunigraz.at>
2018-02-15 15:49:56 +0100

    Hello World function added that Writes out Helloooooo Woooooorld

--- acres/Functions.py
+++ acres/Functions.py
@@ -13,7 +13,7 @@ from random import randint
 ############################################
 def HelloWorld:
     #This is a Hello World function
-    print("Hellooooo Woooooorld")
+    print("Helliiii Wiiiirld")
 
 
 ############################################
