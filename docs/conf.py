b4126255d0ef95117795533ac3edaba3bbfcb83a
Michel Oleynik <michelole@gmail.com>
2018-02-17 12:02:17 +0100

    Add source code link to docs
    While we do not have enough documentation, it's better to link to the source code.

--- docs/conf.py
+++ docs/conf.py
@@ -43,6 +43,7 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.todo',
     'sphinx.ext.coverage',
+    'sphinx.ext.viewcode',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
