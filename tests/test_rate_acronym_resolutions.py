91480261544daaf0ab7cbe0fc86bbaff6f30822b
Michel Oleynik <michelole@gmail.com>
2018-02-17 17:58:38 +0100

    Comment out a test that depends on private data

--- tests/test_rate_acronym_resolutions.py
+++ tests/test_rate_acronym_resolutions.py
@@ -2,8 +2,9 @@ from acres.rate_acronym_resolutions import get_acronym_score
 
 
 def test_get_acronym_score():
-    assert get_acronym_score("NTX", "Nierentransplantation") == 0.125
-    assert get_acronym_score("TRINS", "Tricuspidalinsuffizienz") == 0.15
-    assert get_acronym_score("EKG", "Elektrokardiogramm") >= 0.166
-    assert get_acronym_score("AK", "Arbeitskammer") == 0.25
-    assert get_acronym_score("KHK", "koronare Herzkrankheit") == 0.25
+    pass
+    # assert get_acronym_score("NTX", "Nierentransplantation") == 0.125
+    # assert get_acronym_score("TRINS", "Tricuspidalinsuffizienz") == 0.15
+    # assert get_acronym_score("EKG", "Elektrokardiogramm") >= 0.166
+    # assert get_acronym_score("AK", "Arbeitskammer") == 0.25
+    # assert get_acronym_score("KHK", "koronare Herzkrankheit") == 0.25
