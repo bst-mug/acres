72027744ce062551d28186803fb157c4884483c3
Michel Oleynik <michelole@gmail.com>
2018-02-17 17:45:51 +0100

    Add tests for acronym score
    As a plus, add function overload to simplify usage.

--- tests/test_rate_acronym_resolutions.py
+++ tests/test_rate_acronym_resolutions.py
@@ -1,5 +1,9 @@
-from .context import rate_acronym_resolutions
+from acres.rate_acronym_resolutions import get_acronym_score
 
 
-def test_GetAcronymScore():
-    assert True == True
+def test_get_acronym_score():
+    assert get_acronym_score("NTX", "Nierentransplantation") == 0.125
+    assert get_acronym_score("TRINS", "Tricuspidalinsuffizienz") == 0.15
+    assert get_acronym_score("EKG", "Elektrokardiogramm") >= 0.166
+    assert get_acronym_score("AK", "Arbeitskammer") == 0.25
+    assert get_acronym_score("KHK", "koronare Herzkrankheit") == 0.25
