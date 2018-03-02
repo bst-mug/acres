8f86c8569cf0d0c0ad121d50396985160be7e064
Stefan Schulz <stefan.schulz@medunigraz.at>
2018-03-02 18:50:09 +0100

    Added creation of ngram model directly out of the corpus, with robust import (suppresses UTF-8 encoding error that sometime occurs) Still to be discussed how unit testing handled.

--- acres/create_dumps.py
+++ acres/create_dumps.py
@@ -12,47 +12,99 @@ logger.setLevel(logging.INFO)
 
 # logger.setLevel(logging.DEBUG) # Uncomment this to get debug messages
 
-
-def create_normalised_token_dump():
+def create_corpus_char_stat_dump(corpus_path, ngramlength=8, digit_placeholder="Ð", break_marker="¶"):
     """
-    Creates a set of all tokens in the ngram table, taking into account all possible variants
-    typical for clinical German.
+    - Takes a corpus consisting of text files in a single directory
+    - Substitutes digits and line breaks
+    - Generates a statistics of character ngrams including the digit and break substitutes
+    - Purpose: To substitute artificial breaks in a corpus
+    """
+    counter = 0
+    texts = functions.robust_text_import_from_dir(corpus_path)
+    dict_char_ngrams = {}
+    # this produces a file with character ngrams
+    # at the end also a pickle dump is created
+    f = open("models/ngrams/character_ngrams.txt", 'w', encoding="UTF-8")
+    for text in texts:
+        str_doc = ""
+        lines = text.split("\n")
+        for line in lines:
+            print(line)
+            line = functions.clear_digits(line, digit_placeholder)
+            str_doc = str_doc + line.strip() + break_marker
+        for i in range(0, len(str_doc) - (ngramlength - 1)):
+            ngram = str_doc[0 + i: ngramlength + i].strip()
+            if len(ngram) == ngramlength:
+                print(ngram)
+                if ngram not in dict_char_ngrams:
+                    dict_char_ngrams[ngram] = 1
+                else:
+                    dict_char_ngrams[ngram] += 1
+    out = []
+    for n in dict_char_ngrams:
+        out.append("{:10}".format(dict_char_ngrams[n]) + "\t" + n)
 
-    XXX Check whether it's used !!
-    INCOMLETE RULESET FOR TRANSFORMING 8 bit to 7 bit
-    including normalizing K-C-Z (only relevant for Medical German)
-    XXX Soundex ?
-    XXX similar function elsewhere?
+    out.sort(reverse=True)
+    for line in out:
+        f.write(line + "\n")
+        counter += 1
+    f.close()
+    pickle.dump(dict_char_ngrams, open("models/pickle/character_ngrams.p", "wb"))
+    return (counter)  # should return 70980 with sample docs
 
+
+# print(create_corpus_char_stat_dump(functions.import_conf("SAMPLEPATH")))
+# TODO : we have to assure that unit tests with small input data do not
+# TODO : overwrite the models (ngram and pickle)
+
+def create_corpus_ngramstat_dump(corpus_path, fix_lines=True, min_length=1, max_length=7, digit_placeholder="Ð",
+                                 break_marker="¶"):
+    """
+    Takes a corpus consisting of text files in a single directory
+    Substitutes digits and line breaks
+    It requires that all documents are in UTF-8 text.
+    It can perform line break cleansing (removes artificial line breaks)
+    and substitutions of digits.
+    For fixing the lines, a character ngram stat dictionary, CREATED FROM THE SAME OR A SIMILAR
+    CORPUS, character_ngrams.p  must be in place.
     :return:
     """
-    ngram_stat = functions.import_conf("NGRAMFILE")
-    # "..\\..\\stat\corpus_cardio_training_cleaned_1_to_7gram_stat.txt"
-    # ngram statistics representing a specific document genre and domain
-    logger.debug(ngram_stat)
+    entire_corpus = ""
+    counter = 0
+    texts = functions.robust_text_import_from_dir(corpus_path)
 
-    all_tokens = set()
-    all_token_variants = set()
-    with open(ngram_stat) as file:
-        for row in file:
-            row = row.replace("-", " ").replace("/", " ").replace("(", " ").replace(")", " ")
-            tokens = row.split(" ")
-            for token in tokens:
-                all_tokens.add(token)
-        for token in all_tokens:
-            token = token.replace(".", "").replace(",", "").replace(";", "")
-            token = token.replace(":", "").replace("!", "").replace("?", "")
+    for text in texts:
+        if fix_lines:
+            dict_char_ngrams = pickle.load(open("models/pickle/character_ngrams.p", "rb"))
+            text = functions.fix_line_endings(text, dict_char_ngrams, break_marker)
+        if len(digit_placeholder) == 1:
+            text = functions.clear_digits(
+                text, digit_placeholder)
+        text = text.replace("  ", " ").replace("  ", " ")
+        text = text.replace(break_marker, " " + break_marker + " ")
+        entire_corpus = entire_corpus + text + "\n\n"
+        counter += 1
 
-            all_token_variants.add(token)
-            all_token_variants.add(token.lower())
-            token = token.replace("k", "c").replace("z", "c")
-            all_token_variants.add(token)
-            all_token_variants.add(token.lower())
-            token = token.replace("ä", "ae").replace(
-                "ö", "oe").replace("ü", "ue").replace("ß", "ss")
-            all_token_variants.add(token)
-            all_token_variants.add(token.lower())
-    pickle.dump(all_token_variants, open("tokens.p", "wb"))
+    logger.debug("Corpus loaded containing %d documents.", counter)
+    dict_ngramstat = functions.create_ngram_statistics(entire_corpus, min_length, max_length)
+    lst_ngramstat = []
+    for key in dict_ngramstat:
+        lst_ngramstat.append("{:10}".format(dict_ngramstat[key]) + "\t" + key)
+        counter += 1
+    lst_ngramstat.sort(reverse=True)
+    f = open("models/ngrams/token_ngrams.txt", 'w', encoding="UTF-8")
+    for line in lst_ngramstat:
+        f.write(line + "\n")
+    f.close()
+    pickle.dump(dict_ngramstat, open("models/pickle/token_ngrams.p", "wb"))
+    return (counter)
+
+
+print(create_corpus_ngramstat_dump(functions.import_conf("SAMPLEPATH")))
+
+1 / 0
+
+
 
 
 def create_ngramstat_dump(ngram_stat_filename, ngramstat, min_freq):
@@ -136,6 +188,48 @@ def create_ngramstat_dump(ngram_stat_filename, ngramstat, min_freq):
     pickle.dump(index, open("models/pickle/index.p", "wb"))
 
 
+def create_normalised_token_dump():
+    """
+    Creates a set of all tokens in the ngram table, taking into account all possible variants
+    typical for clinical German.
+
+    XXX Check whether it's used !!
+    INCOMLETE RULESET FOR TRANSFORMING 8 bit to 7 bit
+    including normalizing K-C-Z (only relevant for Medical German)
+    XXX Soundex ?
+    XXX similar function elsewhere?
+
+    :return:
+    """
+    ngram_stat = functions.import_conf("NGRAMFILE")
+    # "..\\..\\stat\corpus_cardio_training_cleaned_1_to_7gram_stat.txt"
+    # ngram statistics representing a specific document genre and domain
+    logger.debug(ngram_stat)
+
+    all_tokens = set()
+    all_token_variants = set()
+    with open(ngram_stat) as file:
+        for row in file:
+            row = row.replace("-", " ").replace("/", " ").replace("(", " ").replace(")", " ")
+            tokens = row.split(" ")
+            for token in tokens:
+                all_tokens.add(token)
+        for token in all_tokens:
+            token = token.replace(".", "").replace(",", "").replace(";", "")
+            token = token.replace(":", "").replace("!", "").replace("?", "")
+
+            all_token_variants.add(token)
+            all_token_variants.add(token.lower())
+            token = token.replace("k", "c").replace("z", "c")
+            all_token_variants.add(token)
+            all_token_variants.add(token.lower())
+            token = token.replace("ä", "ae").replace(
+                "ö", "oe").replace("ü", "ue").replace("ß", "ss")
+            all_token_variants.add(token)
+            all_token_variants.add(token.lower())
+    pickle.dump(all_token_variants, open("tokens.p", "wb"))
+
+
 def create_acro_dump():
     """
     Creates and dumps set of acronyms from ngram statistics.
@@ -201,83 +295,6 @@ def create_morpho_dump():
 
     pickle.dump(s_morph, open("models/pickle/morphemes.p", "wb"))
 
-
-def create_corpus_char_stat_dump(corpus_path, ngramlength=8, digit_placeholder="Ð", break_marker="¶"):
-    """
-
-    Creates a character ngram statistics from a set of text files
-    FIXME: if running this with the whole set of training data it produces an error always
-    'charmap' codec can't decode byte 0x81 in position 3417: character maps to <undefined>
-    This always happens with the same file
-    We have to check this with the local files.
-    So far, use the existing token ngram model
-
-    """
-    counter = 0
-    texts = functions.robust_text_import_from_dir(corpus_path)
-    dict_char_ngrams = {}
-    f = open("models/ngrams/character_ngrams.txt", 'w', encoding="UTF-8")
-    for text in texts:
-        print(text)
-        str_doc = ""
-        counter += 1
-        for line in text:
-            line = functions.clear_digits(line, digit_placeholder)
-            str_doc = str_doc + line.strip() + break_marker
-        for i in range(0, len(line) - (ngramlength - 1)):
-            ngram = line[0 + i: ngramlength + i].strip()
-            if len(ngram) == ngramlength:
-                if ngram not in dict_char_ngrams:
-                    dict_char_ngrams[ngram] = 1
-                else:
-                    dict_char_ngrams[ngram] += 1
-
-    # write the ngrams into text file
-    out = []
-    for n in dict_char_ngrams:
-        out.append("{:10}".format(dict_char_ngrams[n]) + "\t" + n)
-
-    out.sort(reverse=True)
-    for line in out:
-        f.write(line + "\n")
-    f.close()
-    pickle.dump(dict_char_ngrams, open(
-        "models/pickle/character_ngrams.p", "wb", encoding="UTF-8"))
-
-
-# create_corpus_char_stat_dump("SAMPLEPATH")
-
-# 1 / 0
-
-def create_corpus_ngramstat_dump(corpus_path, fix_lines=True, digit_placeholder="Ð", break_marker="¶"):
-    """
-    Takes the path with the corpus.
-    It requires that all documents are in UTF-8 text.
-    It can perform line break cleansing (removes artificial line breaks)
-    and substitutions of digits.
-    For fixing the lines, a character ngram stat dictionary, CREATED FROM THE SAME OR A SIMILAR
-    CORPUS, character_ngrams.p  must be in place.
-
-    :return:
-    """
-    entire_corpus = ""
-    counter = 0
-    texts = functions.robust_text_import_from_dir(corpus_path)
-    for text in texts:
-        if fix_lines:
-            dict_char_ngrams = pickle.load(open("pickle//character_ngrams.p", "rb"))
-            text = functions.fix_line_endings(text, dict_char_ngrams, "break_marker")
-        if len(digit_placeholder) == 1:
-            text = functions.clear_digits(
-                text, digit_placeholder)
-        text = text.replace("  ", " ").replace("  ", " ")
-        text = text.replace(
-            break_marker, " " + break_marker + " ")
-        entire_corpus = entire_corpus + text + "\n\n"
-        counter += 1
-
-    logger.debug("Corpus loaded containing %d documents.", counter)
-    # = pickle.load(open("models/pickle/character_ngrams.p", "rb"))
 
 
 # create_corpus_ngramstat_dump()
