a2a41fa1b8600672b06d6fa8d6e1dfc5e215431a
Michel Oleynik <michelole@gmail.com>
2018-03-08 20:00:51 +0100

    Enable test for find_embeddings

--- tests/test_get_synonyms_from_ngrams.py
+++ tests/test_get_synonyms_from_ngrams.py
@@ -1,16 +1,20 @@
+from acres import get_synonyms_from_ngrams
+from acres import resource_factory
+
+
 def test_find_embeddings():
-    pass
-    # ngrams = resource_factory.get_ngramstat()
-    # index = resource_factory.get_index()
-    #
-    # min_window_size = 5
-    # freq = 1
-    # max_count = 100
-    # min_num_tokens = 6
-    # max_num_tokens = 10
-    #
-    # actual = find_embeddings("re", "OL", "", ngrams, index, min_window_size, freq, max_count, min_num_tokens,
-    #                          max_num_tokens)
-    # expected = []
-    #
-    # assert expected == actual
+    ngrams = resource_factory.get_ngramstat()
+    index = resource_factory.get_index()
+
+    min_window_size = 5
+    freq = 1
+    max_count = 100
+    min_num_tokens = 6
+    max_num_tokens = 10
+
+    actual = get_synonyms_from_ngrams.find_embeddings("re", "OL", "", ngrams, index, min_window_size, freq, max_count,
+                                                      min_num_tokens,
+                                                      max_num_tokens)
+    expected = []
+
+    assert expected == actual
