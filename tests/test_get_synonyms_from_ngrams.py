cc3653ce75fdfbb0e8414ad901afad5ed7faf292
Michel Oleynik <michelole@gmail.com>
2018-06-25 13:43:04 +0200

    Improve unit tests

--- tests/test_get_synonyms_from_ngrams.py
+++ tests/test_get_synonyms_from_ngrams.py
@@ -2,14 +2,23 @@ from acres import get_synonyms_from_ngrams
 
 
 def test_find_embeddings():
-    actual = get_synonyms_from_ngrams.find_embeddings("re", "OL", "<VOID>", 5, 1, 100, 6, 10)
-    expected = []
-    assert expected == actual
-
-    actual = get_synonyms_from_ngrams.find_embeddings("<SEL>", "PDU", "<SEL>", 10, 3, 50, 1, 5)
-    expected = []
-    assert expected == actual
-
-    actual = get_synonyms_from_ngrams.find_embeddings("<SEL>", "EKG", "<SEL>", 10, 3, 50, 1, 5)
+    # Explicit context
+    actual = get_synonyms_from_ngrams.find_embeddings("*", "EKG", "¶", 10, 1, 100, 1, 5)
     expected = ['0000019\tPhysikalischer Status']
     assert set(expected).issubset(actual)
+
+    # Relax right context
+    actual = get_synonyms_from_ngrams.find_embeddings("*", "EKG", "<SEL>", 10, 1, 100, 1, 5)
+    expected = ['0000019\tPhysikalischer Status']
+    assert set(expected).issubset(actual)
+
+    # Relax left and right contexts
+    actual = get_synonyms_from_ngrams.find_embeddings("<SEL>", "EKG", "<SEL>", 10, 1, 100, 1, 5)
+    expected = ['0000019\tPhysikalischer Status']
+    assert set(expected).issubset(actual)
+
+    # Changing min_num_tokens should restrict results
+    # "* EKG ¶", the only valid embedding, happens 27 times
+    actual = get_synonyms_from_ngrams.find_embeddings("<SEL>", "EKG", "<SEL>", 10, 28, 100, 1, 5)
+    expected = ['0000019\tPhysikalischer Status']
+    assert not set(expected).issubset(actual)
