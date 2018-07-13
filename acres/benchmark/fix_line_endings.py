3b1926a1af0dae9bbfb0096a150395ced7ef8264
Stefan Schulz <stefan.schulz@medunigraz.at>
2018-07-13 15:49:09 +0200

    Fixing line ending stuff

---
+++ acres/benchmark/fix_line_endings.py
@@ -0,0 +1,18 @@
+from acres import functions
+
+
+def test_fine_ending_fixings():
+    actual_input_text = """* Anamnese und klin. Symptomatik
+
+Die Vorstellung des Pat. in der EBA für Innere Medizin
+erfolgte aufgrund einer schmerzhaften Schwellung des li.
+Oberschenkels. Eine Eigenanamnese ist mit dem Pat. nicht erhebbar. Laut
+telef. Rücksprache mit dem Heim ist kein Sturz erhebbar.
+
+* Physikalischer Status
+
+Temp 36,3°, RR initial 198/90 nach 2 Hb Nitro 165,82
+C: nc, arrh, leise
+P iL bei < Compliance : VA
+Abd.palp. unauff, lieg. PEG"""
+    print(functions.fix_line_endings(actual_input_text, "\n"))
