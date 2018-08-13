f8176d6a984e523cf95b218e3f10f15b7cc02a89
Michel Oleynik <michelole@gmail.com>
2018-08-13 18:25:37 +0200

    Move `find_embeddings` tests to `benchmark`

--- tests/benchmark/test_benchmark.py
+++ tests/benchmark/test_benchmark.py
@@ -1,3 +1,4 @@
+from acres.ngram import finder
 from acres.preprocess import resource_factory
 from acres.util import text
 
@@ -35,3 +36,81 @@ Abd.palp. unauff, lieg. PEG
 
     actual = text.fix_line_endings(doc, "\n")
     assert expected == actual
+
+
+def test_find_embeddings():
+    actual = finder.find_embeddings("nach", "ICD", "Implantation", 1, 2, 500, 1, 10)
+    expected = [(76, 'CRT/ICD'), (50, 'prophylaktischer CRT/ICD'), (42, 'prophylaktischer ICD')]
+    assert set(expected).issubset(actual)
+
+    actual = finder.find_embeddings("<SEL>", "HF-Anstieg", "von", 1, 2, 500, 1, 10)
+    expected = []
+    assert set(expected).issubset(actual)
+
+    actual = finder.find_embeddings("<SEL>", "HT", "rein", 1, 2, 500, 1, 10)
+    expected = []
+    assert set(expected).issubset(actual)
+
+    # viele Treffer, die mit Ht anfangen
+    actual = finder.find_embeddings("geplanten", "EPU", "*", 1, 2, 500, 1, 10)
+    expected = []
+    assert set(expected).issubset(actual)
+
+    actual = finder.find_embeddings("einem", "EDP", "von", 1, 2, 500, 1, 10)
+    expected = [(1737, 'max. Gradienten'), (710, 'mittleren Gradienten'), (325, 'LVEDD')]
+    assert set(expected).issubset(actual)
+
+    # wird nicht gefunden
+    actual = finder.find_embeddings("gutem", "AZ", "nach", 1, 2, 100, 1, 10)
+    expected = [(311, 'AZ und mit blander Punktionsstelle'), (277, 'AZ wieder'),
+                (140, 'AZ und bei blander Punktionsstelle')]
+    assert set(expected).issubset(actual)
+
+    actual = finder.find_embeddings("die", "VCS.", "<SEL>", 1, 2, 500, 1, 10)
+    expected = []
+    assert set(expected).issubset(actual)
+
+    actual = finder.find_embeddings("<SEL>", "DG's", "<SEL>", 1, 2, 500, 1, 10)
+    expected = []
+    assert set(expected).issubset(actual)
+
+    # only without restricted context the resolution is found
+    # only DG's resolved, not DGs
+    actual = finder.find_embeddings("die", "VCS.", "<SEL>", 1, 2, 500, 1, 10)
+    expected = []
+    assert set(expected).issubset(actual)
+
+    # only works with final dot!
+    actual = finder.find_embeddings("re", "OL", "<SEL>", 1, 2, 500, 1, 10)
+    expected = []
+    assert set(expected).issubset(actual)
+
+    actual = finder.find_embeddings("die", "VCS", "<VOID>", 3, 1, 100, 1, 10)
+    expected = []
+    assert set(expected).issubset(actual)
+
+    # Code originally commented out below #
+
+    actual = finder.find_embeddings("", "morph.", "", 10, 3, 1000, 1, 7)
+    expected = []
+    assert set(expected).issubset(actual)
+
+    actual = finder.find_embeddings("Mitralklappe", "morph.", "*", 10, 3, 1000, 1, 7)
+    expected = []
+    assert set(expected).issubset(actual)
+
+    actual = finder.find_embeddings("", "morph.", "", 18, 3, 1000, 1, 1)
+    expected = []
+    assert set(expected).issubset(actual)
+
+    actual = finder.find_embeddings("", "morph.", "unauff.", 18, 3, 1000, 3, 7)
+    expected = []
+    assert set(expected).issubset(actual)
+
+    actual = finder.find_embeddings("<SEL>", "ms", "<SEL>", 8, 30, 500, 1, 5)
+    expected = []
+    assert set(expected).issubset(actual)
+
+    actual = finder.find_embeddings("Ð,Ð", "ms", "", 8, 3, 500, 1, 7)
+    expected = []
+    assert set(expected).issubset(actual)
