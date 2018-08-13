bd5410efd75ba10094005e0789706fbd38d13555
Michel Oleynik <michelole@gmail.com>
2018-08-13 12:24:36 +0200

    Rename fixture to a more descriptive name
    So that it can be overriden.

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,7 +4,7 @@ from acres.preprocess import resource_factory
 
 
 @pytest.fixture(scope="session", autouse=True)
-def execute_before_any_test():
+def path_resource_factory():
     resource_factory.PICKLE_FOLDER = "tests/" + resource_factory.PICKLE_FOLDER
     resource_factory.NGRAMS_FOLDER = "tests/" + resource_factory.NGRAMS_FOLDER
     resource_factory.LOG_FOLDER = "tests/" + resource_factory.LOG_FOLDER
