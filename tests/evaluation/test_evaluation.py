586800271529fe919769d5b5ec6eeb980461e4e2
Michel Oleynik <michelole@gmail.com>
2018-08-14 18:56:16 +0200

    Add baseline test for evaluation

--- tests/evaluation/test_evaluation.py
+++ tests/evaluation/test_evaluation.py
@@ -1,5 +1,7 @@
 from acres.evaluation import evaluation
 
 
-def test_dummy():
-    assert True
+def test_analyze_file():
+    (precision, recall) = evaluation.analyze_file("tests/resources/workbench.tsv")
+    assert 0.0 == precision
+    assert 0.0 == recall
