99077e85d887c46479ac37678b1fe25bfcd72d19
Michel Oleynik <michelole@gmail.com>
2018-08-17 12:35:53 +0200

    Update test expectations after 6abce531db4e21ef023a8433ca54f51edd63c65c

--- tests/test_rater.py
+++ tests/test_rater.py
@@ -10,8 +10,8 @@ def test_get_acronym_score():
     assert 0 == get_acronym_score("AE", "a b c d e")[1]  # Short acronym
     assert 0 == get_acronym_score("ABCDEL", "a b c d e f g h i j k ll")[1]  # Long acronym
 
-    assert 0.5 == get_acronym_score("NTX", "Nierentransplantation")[1]
-    assert 0.5 == get_acronym_score("TRINS", "Tricuspidalinsuffizienz")[1]
+    assert 0.2 == get_acronym_score("NTX", "Nierentransplantation")[1]
+    assert 1.0 == get_acronym_score("TRINS", "Tricuspidalinsuffizienz")[1]
     assert 1.0 == get_acronym_score("EKG", "Elektrokardiogramm")[1]
     assert 1.0 == get_acronym_score("AK", "Arbeitskammer")[1]
     assert 1.0 == get_acronym_score("KHK", "koronare Herzkrankheit")[1]
