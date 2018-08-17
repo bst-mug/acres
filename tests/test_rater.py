3d99662a048dd1ec67d6bb4133e68d94bfe1d2ec
Michel Oleynik <michelole@gmail.com>
2018-08-17 12:31:07 +0200

    Fix AssertionError due to changed function prototype

--- tests/test_rater.py
+++ tests/test_rater.py
@@ -2,16 +2,16 @@ from acres.rater import get_acronym_score
 
 
 def test_get_acronym_score():
-    assert get_acronym_score("A", "Ambulanz") == 0  # Acronym too short
-    assert get_acronym_score("Aa", "Am") == 0  # Full form too short
-    assert get_acronym_score("AM", "AMbulanz") == 0  # Acronym within full form
+    assert get_acronym_score("A", "Ambulanz")[1] == 0  # Acronym too short
+    assert get_acronym_score("Aa", "Am")[1] == 0  # Full form too short
+    assert get_acronym_score("AM", "AMbulanz")[1] == 0  # Acronym within full form
 
     # Relative length do not match
-    assert get_acronym_score("AE", "a b c d e") == 0  # Short acronym
-    assert get_acronym_score("ABCDEL", "a b c d e f g h i j k ll") == 0  # Long acronym
+    assert get_acronym_score("AE", "a b c d e")[1] == 0  # Short acronym
+    assert get_acronym_score("ABCDEL", "a b c d e f g h i j k ll")[1] == 0  # Long acronym
 
-    assert get_acronym_score("NTX", "Nierentransplantation") == 0.5
-    assert get_acronym_score("TRINS", "Tricuspidalinsuffizienz") == 0.5
-    assert get_acronym_score("EKG", "Elektrokardiogramm") == 1.0
-    assert get_acronym_score("AK", "Arbeitskammer") == 1.0
-    assert get_acronym_score("KHK", "koronare Herzkrankheit") == 1.0
+    assert get_acronym_score("NTX", "Nierentransplantation")[1] == 0.5
+    assert get_acronym_score("TRINS", "Tricuspidalinsuffizienz")[1] == 0.5
+    assert get_acronym_score("EKG", "Elektrokardiogramm")[1] == 1.0
+    assert get_acronym_score("AK", "Arbeitskammer")[1] == 1.0
+    assert get_acronym_score("KHK", "koronare Herzkrankheit")[1] == 1.0
