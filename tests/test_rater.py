68af5483ea3680f85ed877694f44fe06919bb26e
Michel Oleynik <michelole@gmail.com>
2018-08-17 12:32:48 +0200

    Make expected value the first one

--- tests/test_rater.py
+++ tests/test_rater.py
@@ -2,16 +2,16 @@ from acres.rater import get_acronym_score
 
 
 def test_get_acronym_score():
-    assert get_acronym_score("A", "Ambulanz")[1] == 0  # Acronym too short
-    assert get_acronym_score("Aa", "Am")[1] == 0  # Full form too short
-    assert get_acronym_score("AM", "AMbulanz")[1] == 0  # Acronym within full form
+    assert 0 == get_acronym_score("A", "Ambulanz")[1]  # Acronym too short
+    assert 0 == get_acronym_score("Aa", "Am")[1]  # Full form too short
+    assert 0 == get_acronym_score("AM", "AMbulanz")[1]  # Acronym within full form
 
     # Relative length do not match
-    assert get_acronym_score("AE", "a b c d e")[1] == 0  # Short acronym
-    assert get_acronym_score("ABCDEL", "a b c d e f g h i j k ll")[1] == 0  # Long acronym
+    assert 0 == get_acronym_score("AE", "a b c d e")[1]  # Short acronym
+    assert 0 == get_acronym_score("ABCDEL", "a b c d e f g h i j k ll")[1]  # Long acronym
 
-    assert get_acronym_score("NTX", "Nierentransplantation")[1] == 0.5
-    assert get_acronym_score("TRINS", "Tricuspidalinsuffizienz")[1] == 0.5
-    assert get_acronym_score("EKG", "Elektrokardiogramm")[1] == 1.0
-    assert get_acronym_score("AK", "Arbeitskammer")[1] == 1.0
-    assert get_acronym_score("KHK", "koronare Herzkrankheit")[1] == 1.0
+    assert 0.5 == get_acronym_score("NTX", "Nierentransplantation")[1]
+    assert 0.5 == get_acronym_score("TRINS", "Tricuspidalinsuffizienz")[1]
+    assert 1.0 == get_acronym_score("EKG", "Elektrokardiogramm")[1]
+    assert 1.0 == get_acronym_score("AK", "Arbeitskammer")[1]
+    assert 1.0 == get_acronym_score("KHK", "koronare Herzkrankheit")[1]
