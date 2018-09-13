fdb06286f45e32be2d5af7994ff117c8d959609f
Michel Oleynik <michelole@users.noreply.github.com>
2018-09-13 20:26:01 +0200

    Fix build

--- tests/evaluation/test_evaluation.py
+++ tests/evaluation/test_evaluation.py
@@ -2,6 +2,6 @@ from acres.evaluation import evaluation
 
 
 def test_analyze_file(ngramstat):
-    (precision, recall) = evaluation.analyze_file("tests/resources/workbench.tsv")
+    (precision, recall) = evaluation.analyze_file("tests/resources/workbench.tsv", evaluation.Strategy.WORD2VEC)
     assert 0.0 == precision
     assert 0.0 == recall
