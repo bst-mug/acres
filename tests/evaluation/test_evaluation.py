f73ac895a6055a5e9513c1679f345aa342ac69ce
Michel Oleynik <michelole@gmail.com>
2018-09-18 21:25:01 +0200

    Add hit on test workbench
    So that we have at least one valid expansion...

--- tests/evaluation/test_evaluation.py
+++ tests/evaluation/test_evaluation.py
@@ -3,5 +3,5 @@ from acres.evaluation import evaluation
 
 def test_analyze_file(ngramstat):
     (precision, recall) = evaluation.analyze_file("tests/resources/workbench.tsv", evaluation.Strategy.WORD2VEC)
-    assert 0.0 == precision
-    assert 0.0 == recall
+    assert 1.0 == precision
+    assert 0.25 == recall
