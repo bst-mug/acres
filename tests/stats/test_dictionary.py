f1a7dae75fbf41af08ed884ec5f74a97b97cfff6
Michel Oleynik <michelole@gmail.com>
2018-09-18 18:48:34 +0200

    Add unit tests for `dictionary.py`

---
+++ tests/stats/test_dictionary.py
@@ -0,0 +1,21 @@
+from acres.stats import dictionary
+
+
+def test_dump_sample():
+    expected = ['AA\tAlopecia areata', 'γ-GT\tγ-Glutamyltransferase']
+    actual = dictionary.dump_sample()
+    assert set(expected).issubset(actual)
+
+
+def test_show_extremes(capsys):
+    dictionary.show_extremes("", [], 1, 1)
+    expected = "List too small"
+    captured = capsys.readouterr()
+    actual = captured.out.split("\n")[-2]
+    assert expected == actual
+
+    dictionary.show_extremes("", ["a", "b", "c"], 1, 1)
+    expected = ["a", "(...)", "c"]
+    captured = capsys.readouterr()
+    actual = captured.out.split("\n")[-4:-1]
+    assert expected == actual
