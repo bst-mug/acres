04d4eb75dc3bf886be87abea49835fa0d152b166
Michel Oleynik <michelole@gmail.com>
2018-09-19 11:42:19 +0200

    Update check script
    - Add environment switch
    - Separate benchmark tests
    - Move unit tests to the end, as they take longer

--- scripts/check.sh
+++ scripts/check.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
+echo "Switching environment..."
+pyenv activate python-3.7
+
 echo "Checking for syntax errors..."
 pylint -E acres
 
 echo "Checking for pylint warnings..."
 pylint -d R,C acres
-
-echo "Running tests..."
-pytest -q --log-level=ERROR
 
 echo "Checking static typing..."
 mypy --strict --ignore-missing-imports acres
@@ -17,3 +17,9 @@ sphinx-apidoc -fT -o docs acres > /dev/null
 
 echo "Generating docs..."
 sphinx-build -W -b html docs/ docs/_build/ > /dev/null
+
+echo "Running not benchmark tests"
+pytest -q --log-level=ERROR -k "not benchmark"
+
+echo "Running benchmark tests..."
+pytest -q --log-level=ERROR -k "benchmark"
