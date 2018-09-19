b25b5f356db09322721b3a7476423d537a6213e7
Michel Oleynik <michelole@gmail.com>
2018-09-19 12:27:51 +0200

    Update test method name to better reflect reality

--- tests/web/test_azure.py
+++ tests/web/test_azure.py
@@ -9,7 +9,7 @@ def test_get_web_corpus():
     assert expected == actual
 
 
-def test_get_web_results():
+def test_cached_get_web_results():
     expected = "Elektrokardiogramm – Wikipedia"
     actual = azure.cached_get_web_results("EKG")
     assert expected in [result.name for result in actual]
