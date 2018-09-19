9dfbb273f14b6f93ebe33387840969882e0f6b38
Michel Oleynik <michelole@gmail.com>
2018-09-19 13:38:20 +0200

    Add tests for non-cached azure results using monkeypatch

--- tests/web/test_azure.py
+++ tests/web/test_azure.py
@@ -8,6 +8,10 @@ def test_get_web_corpus():
     actual = azure.get_web_corpus("akjkjgehwvmewm")
     assert expected == actual
 
+    expected = "Elektrokardiogramm"
+    actual = azure.get_web_corpus("EKG")
+    assert expected in actual
+
 
 def test_cached_get_web_results():
     expected = "Elektrokardiogramm – Wikipedia"
@@ -18,3 +22,14 @@ def test_cached_get_web_results():
     expected = None
     actual = azure.cached_get_web_results("akjkjgehwvmewm")
     assert expected == actual
+
+
+def test_get_web_results(monkeypatch, mock_get_url):
+    # Monkey patch is_valid_key so that it's always valid
+    def valid_key():
+        return True
+    monkeypatch.setattr(azure, "is_valid_key", valid_key)
+
+    expected = "Elektrokardiogramm – Wikipedia"
+    actual = azure.get_web_results("EKG")
+    assert expected in [result.name for result in actual]
