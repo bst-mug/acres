296fa4b48da975ed2aa22913103f1bd4186cfc16
Michel Oleynik <michelole@gmail.com>
2019-08-20 18:35:03 +0200

    Extract filtering out of resolve method

--- acres/resolution/resolver.py
+++ acres/resolution/resolver.py
@@ -51,14 +51,39 @@ def cached_resolve(acronym: str, left_context: str, right_context: str,
 
     # Check cache entry
     if key not in cache:
-        cache[key] = _resolve(acronym, left_context, right_context, strategy)
+        cache[key] = _filter_resolve(acronym, left_context, right_context, strategy)
 
     return cache[key]
 
 
+def _filter_resolve(acronym: str, left_context: str, right_context: str, strategy: Strategy) -> \
+List[str]:
+    """
+    Resolve a given acronym + context using the provided Strategy and filter out invalid expansions.
+
+    :param acronym:
+    :param left_context:
+    :param right_context:
+    :param strategy:
+    :return:
+    """
+    # TODO Might not be needed after new gold standard (#35)
+    # Get the first acronym of an eventual pair
+    acronym = text.clean(acronym).split()[0]
+
+    expansions = _resolve(acronym, left_context, right_context, strategy)
+
+    filtered_expansions = []
+    for expansion in expansions:
+        if rater.get_acronym_score(acronym, expansion) > 0:
+            filtered_expansions.append(expansion)
+
+    return filtered_expansions
+
+
 def _resolve(acronym: str, left_context: str, right_context: str, strategy: Strategy) -> List[str]:
     """
-    Resolve a given acronym + context using the provideed Strategy.
+    Resolve a given acronym + context using the provided Strategy.
 
     :param acronym:
     :param left_context:
@@ -72,14 +97,4 @@ def _resolve(acronym: str, left_context: str, right_context: str, strategy: Stra
     }
 
     func = switcher.get(strategy)
-
-    # TODO Might not be needed after new gold standard (#35)
-    # Get the first acronym of an eventual pair
-    acronym = text.clean(acronym).split()[0]
-
-    filtered_expansions = []
-    for expansion in func(acronym, left_context, right_context):
-        if rater.get_acronym_score(acronym, expansion) > 0:
-            filtered_expansions.append(expansion)
-
-    return filtered_expansions
+    return func(acronym, left_context, right_context)
