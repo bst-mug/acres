bb63afdc7cd926ac64fb0dc01fb2c5a1de0ae85d
Michel Oleynik <michelole@gmail.com>
2019-09-18 17:26:10 +0200

    Add method to create a topic list

--- acres/model/topic_list.py
+++ acres/model/topic_list.py
@@ -2,7 +2,11 @@
 Model class that represents a topic list.
 """
 from collections import namedtuple
+from operator import attrgetter
 from typing import List, Set
+
+from acres.ngram import ngrams
+from acres.util import functions
 
 Acronym = namedtuple('Acronym', ['acronym', 'left_context', 'right_context'])
 
@@ -41,3 +45,34 @@ def unique_types(topics: List[Acronym]) -> Set[str]:
     for contextualized_acronym in topics:
         types.add(contextualized_acronym.acronym)
     return types
+
+
+def create(filename: str, chance: float, ngram_size: int = 7):
+    """
+    Create a topic list out of random n-grams with a given chance and size.
+
+    :param filename:
+    :param chance:
+    :param ngram_size:
+    :return:
+    """
+    filtered_ngrams = ngrams.FilteredNGramStat(ngram_size)
+    acronyms = ngrams.filter_acronym_contexts(iter(filtered_ngrams))
+    sampled = functions.sample(acronyms, chance)
+
+    # Sort by the acronym
+    sorted_acronyms = sorted(sampled, key=attrgetter('acronym'))
+
+    file = open(filename, "w+", encoding="utf-8")
+    for acronym in sorted_acronyms:
+        file.write(acronym.left_context)
+        file.write("\t")
+        file.write(acronym.acronym)
+        file.write("\t")
+        file.write(acronym.right_context)
+        file.write("\n")
+    file.close()
+
+
+if __name__ == "__main__":
+    create("resources/topic_list.tsv", 0.003)
